60f557a9be65ad17888bc1b64898556014a8a013
Ececsk <ececoskuncay@gmail.com>
2021-03-08 00:09:17 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<_Key,_Value>
+    {
+        _Key[] keys;
+        _Value[] values;
+        public MyDictionary()
+        {
+            keys = new _Key[0];
+            values = new _Value[0];
+        }
+
+        public void Add(_Key key,_Value value)
+        {
+            _Key[] tempKeys = new _Key[keys.Length];
+            _Value[] tempValues = new _Value[values.Length];
+            
+            tempKeys = keys;
+            keys = new _Key[tempKeys.Length + 1];
+
+            tempValues = values;
+            values = new _Value[tempValues.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1]=key;
+            values[values.Length - 1] = value;
+        }
+
+        public void List() 
+        {
+            Console.WriteLine("Plakalar ve Şehirler");
+            for (int i = 0; i < keys.Length-1; i++)
+            {
+                Console.WriteLine(keys[i]+"  "+values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{/*  Yazılım Geliştirici Yetiştirme Kampı - 4.gün - Ödev 5
+ 
+    Derste yaptığımız MyList gibi siz de kendi Dictonary sınıfınızı yazınız. (MyDictionary)
+    Sadece ekleme(Add) metodunu yazınız*/
+    class Program
+    {
+        static void Main(string[] args)
+        { var plakalar = new MyDictionary<int, string>();
+            plakalar.Add(01, "Adana");
+            plakalar.Add(02, "Adıyaman");
+            plakalar.Add(03, "Afyon");
+            plakalar.Add(04, "Ağrı");
+            plakalar.Add(05, "Amasya");
+            plakalar.List();
+                }
+    }
+}
